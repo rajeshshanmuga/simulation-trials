788251f41b51cad98bcdfd413b807783fc8efab0
rajeshshanmugam <rajeshshanmugam@positivenaick.com>
2020-07-22 00:21:35 +0530

    Minor: Add docstrings

--- Risk Analytics/streamlit_app.py
+++ Risk Analytics/streamlit_app.py
@@ -1,10 +1,20 @@
+# Visualising the Model predictions with the dynamic inputs
+
+# Fixme: Change port no
+# Todo: Add Data uploading [feature]
+# Todo: Add Data Exploration [feature]
+# Todo: Live model training [feature]
+# Todo: Add more charts for Model outputs[Enhancement]
+# Todo: Automatic widget finder [Enhancement]
+
 import streamlit as st
 import json
+import pandas as pd
 
 import utils as ut
 import model_prediction as mp
 
-
+# Contains factor details like min, max and values(For categories)
 with open("data/factors.json") as file:
     factors = json.load(file)
 
@@ -20,7 +30,7 @@ credit_amount = [values['range'] for values in factors if values['factor_name'] 
 age = [values['range'] for values in factors if values['factor_name'] == 'Age']
 duration = [values['range'] for values in factors if values['factor_name'] == 'Duration in Months']
 
-
+# Creating widgets
 sex = st.sidebar.selectbox('Sex', sex_values[0])
 housing = st.sidebar.selectbox('Housing', housing_values[0])
 saving_accounts = st.sidebar.selectbox('Saving account', savings_accounts[0])
@@ -37,12 +47,11 @@ duration = st.sidebar.slider(label='Duration in Months', min_value=duration[0][0
 # Organises the input factors for the prediction
 factors = ut.factors_organiser(age, sex, job, housing, saving_accounts, checking_account,
                                    ca, duration, purpose)
+# Model prediction
 out = mp.prob_prdictor(factors)
-import pandas as pd
+
 out = pd.DataFrame.from_dict(out, orient='index')
-print(out)
 st.bar_chart(data=out)
-#     return out
 
 
 
